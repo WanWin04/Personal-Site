afc225e2f47b2886a31324b5deafaffc83193abc
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2023-11-24 16:36:26 +0700

    Design Header - CSS and TSX

--- src/Components/Header/HeaderSocials.tsx
+++ src/Components/Header/HeaderSocials.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { LuInstagram } from "react-icons/lu";
+import { FaFacebook } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com" target='_blank'></a>
-        <a href="https://github.com" target='_blank'></a>
-        <a href="https://www.instagram.com" target='_blank'></a>
+        <a href="https://linkedin.com" target='_blank'><FaLinkedin /></a>
+        <a href="https://github.com/WanWin04" target='_blank'><FaGithub /></a>
+        <a href="https://www.instagram.com/quangthang_26/" target='_blank'><LuInstagram /></a>
+        <a href="https://web.facebook.com/profile.php?id=100047907622203" target='_blank'><FaFacebook /></a>
     </div>
   )
 }
