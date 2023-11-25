ebea485458e99942148ebecce82c6a7f09c66bb5
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2023-11-25 23:58:25 +0700

    GUI - Add swiper slides

--- src/Components/Testimonials/Testimonials.tsx
+++ src/Components/Testimonials/Testimonials.tsx
@@ -5,6 +5,15 @@ import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
 
+// import Swiper core and required modules
+import { Pagination } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
 const data = [
   {
     avatar: AVTR1,
@@ -21,30 +30,33 @@ const data = [
     name: "Tina",
     review: "YearYearYear",
   },
-]
+];
 
 const Testimonials = () => {
   return (
     <section id="Testimonials">
       <h2>Testimonials</h2>
 
-      <div className="container testimonials_container">
-        {
-          data.map(({avatar, name, review}, index) => {
-            return (
-              <article key={index} className="testimonials">
-                <div className="client_avatar">
-                  <img src={avatar} />
-                </div>
-                <h5 className="client_name">{name}</h5>
-                <small className="client_review">
-                  {review}
-                </small>
-              </article>
-            );
-          })
-        }
-      </div>
+      <Swiper
+        className="container testimonials_container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonials">
+              <div className="client_avatar">
+                <img src={avatar} />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
