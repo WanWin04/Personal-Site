72f44bbcf54a3be7f90405abdb77992fe471bd0c
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2023-11-25 22:03:23 +0700

    GUI - Design Services

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -1,9 +1,110 @@
 import React from "react";
 import "./Services.css";
+import { FaCheck } from "react-icons/fa";
+
 
 const Services = () => {
   return (
-    <section id="Services">Services</section>
+    <section id="Services">
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="services">
+          <div className="services_head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="services_list">
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+
+        <article className="services">
+          <div className="services_head">
+            <h3>BE Design</h3>
+          </div>
+
+          <ul className="services_list">
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+          </ul>
+        </article>
+        {/* END OF BE */}
+
+        <article className="services">
+          <div className="services_head">
+            <h3>BE Design</h3>
+          </div>
+
+          <ul className="services_list">
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+            <li>
+            <FaCheck className="services_list-icon" />
+            <p>YEARSYEARSYEARSYEARS</p>
+            </li>
+
+          </ul>
+        </article>
+        {/* END OF AI */}
+
+      </div>
+
+    </section>
   );
 };
 
