ede33ec2cafe6b4563374aeb575b2a5f90722a94
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2023-11-27 22:41:23 +0700

    GUI - Change Services to Skills

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -5,7 +5,7 @@ import { FaCheck } from "react-icons/fa";
 const Services: React.FC = () => {
   return (
     <section id="Services">
-      <h2>Services</h2>
+      <h2>Skills</h2>
 
       <div className="container services_container">
         <article className="services">
@@ -16,22 +16,7 @@ const Services: React.FC = () => {
           <ul className="services_list">
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
-            </li>
-
-            <li>
-              <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
-            </li>
-
-            <li>
-              <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
-            </li>
-
-            <li>
-              <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>Update later</p>
             </li>
           </ul>
         </article>
@@ -39,28 +24,38 @@ const Services: React.FC = () => {
 
         <article className="services">
           <div className="services_head">
-            <h3>Algorithm</h3>
+            <h3>Programming Languages</h3>
           </div>
 
           <ul className="services_list">
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>C/C++</p>
             </li>
 
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>Python</p>
             </li>
 
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>Javascript/Typescript</p>
             </li>
 
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>HTML/CSS</p>
+            </li>
+
+            <li>
+              <FaCheck className="services_list-icon" />
+              <p>XML/JSON</p>
+            </li>
+
+            <li>
+              <FaCheck className="services_list-icon" />
+              <p>LaTeX</p>
             </li>
           </ul>
         </article>
@@ -68,28 +63,42 @@ const Services: React.FC = () => {
 
         <article className="services">
           <div className="services_head">
-            <h3>Projects</h3>
+            <h3>Frameworks/Tools</h3>
           </div>
 
           <ul className="services_list">
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>Node.js</p>
             </li>
 
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>ReactJS/React Native</p>
             </li>
 
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>Tensorflow</p>
             </li>
 
             <li>
               <FaCheck className="services_list-icon" />
-              <p>YEARSYEARSYEARSYEARS</p>
+              <p>Jupyter Notebook</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF AI */}
+
+        <article className="services">
+          <div className="services_head">
+            <h3>Platform</h3>
+          </div>
+
+          <ul className="services_list">
+            <li>
+              <FaCheck className="services_list-icon" />
+              <p>Leetcode</p>
             </li>
           </ul>
         </article>
